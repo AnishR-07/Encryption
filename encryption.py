155bf6842ed399ef562b76a3d4da6444fee019c7
manishr363 <manishr363@gmail.com>
2025-08-18 00:04:20 +0530

    encryption code

---
+++ encryption.py
@@ -0,0 +1,39 @@
+import random
+import string
+
+sentence = " " + string.punctuation + string.digits +string.ascii_letters
+
+sentence = list(sentence)
+key = sentence.copy()
+
+random.shuffle(key)
+
+#print(sentence)
+#print(key)
+
+#Encryption********************************************************************************
+
+Message = input("Enter your message to enrpyt: ")
+cipher_text = ""
+
+
+for letter in Message:
+    index = sentence.index(letter)
+    cipher_text += key[index]
+
+print(f"Your Original message is :{Message}")
+print(f"Your Encrypted Message is :{cipher_text}")
+
+#Decryption**********************************************************************************
+
+cipher_text = input("Enter your message to enrpyt: ")
+Message = ""
+
+
+for letter in cipher_text:
+    index = key.index(letter)
+    Message += sentence[index]
+
+
+print(f"Your Encrypted Message is :{cipher_text}")
+print(f"Your Decrypted message is :{Message}")
